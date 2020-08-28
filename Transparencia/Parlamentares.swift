3d58764a72b784df3d74eecb23fcb0775f060374
ivan sabino <ivanksabino@gmail.com>
2020-08-28 13:02:30 -0300

    novo json

--- Transparencia/Parlamentares.swift
+++ Transparencia/Parlamentares.swift
@@ -1,20 +1,50 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
 //
-//  Parlamentares.swift
-//  Transparencia
-//
-//  Created by Ivan Sabino on 25/08/20.
-//  Copyright © 2020 Ivan Sabino. All rights reserved.
-//
+//   let parlamentares = try? newJSONDecoder().decode(Parlamentares.self, from: jsonData)
 
 import Foundation
 
-
-
-// MARK: - Parlamentare
-struct Parlamentare: Codable {
-    let index: Int
+// MARK: - Parlamentar
+class Parlamentar: Codable {
     let txNomeParlamentar: String
+    let ideCadastro: Int
     let vlrDocumento: Double
+    let codOrcamento: String?
+    let condicao: Condicao
+    let matricula, idParlamentar: String
+    let urlFoto: String
+    let sexo: Sexo
+    let uf, partido, gabinete, anexo: String
+    let fone, email: String
+
+    init(txNomeParlamentar: String, ideCadastro: Int, vlrDocumento: Double, codOrcamento: String?, condicao: Condicao, matricula: String, idParlamentar: String, urlFoto: String, sexo: Sexo, uf: String, partido: String, gabinete: String, anexo: String, fone: String, email: String) {
+        self.txNomeParlamentar = txNomeParlamentar
+        self.ideCadastro = ideCadastro
+        self.vlrDocumento = vlrDocumento
+        self.codOrcamento = codOrcamento
+        self.condicao = condicao
+        self.matricula = matricula
+        self.idParlamentar = idParlamentar
+        self.urlFoto = urlFoto
+        self.sexo = sexo
+        self.uf = uf
+        self.partido = partido
+        self.gabinete = gabinete
+        self.anexo = anexo
+        self.fone = fone
+        self.email = email
+    }
 }
 
-typealias Parlamentares = [Parlamentare]
+enum Condicao: String, Codable {
+    case suplente = "Suplente"
+    case titular = "Titular"
+}
+
+enum Sexo: String, Codable {
+    case feminino = "feminino"
+    case masculino = "masculino"
+}
+
+typealias Parlamentares = [Parlamentar]
--- Transparencia/ViewController.swift
+++ Transparencia/ViewController.swift
@@ -16,33 +16,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         self.loadJSON { (parlamentares, error) in
             self.parlamentares = parlamentares
-            
-            
             self.tableView.reloadData()
-            for parlamentar in parlamentares{
-                print(parlamentar.txNomeParlamentar)
-            }
         }
-        
-        
-        
+    
     }
     
-    
-    
     func loadJSON(completion: @escaping (Parlamentares, Error?) -> Void){
-        let path =  Bundle.main.path(forResource: "deputadosGastos", ofType: "json")
+        
+        let path =  Bundle.main.path(forResource: "DeputadosGastos", ofType: "json")
         guard let data = try? Data(contentsOf: URL(fileURLWithPath: path!)) else {return}
-
         guard let parlamentares = try? JSONDecoder().decode(Parlamentares.self, from: data) as Parlamentares else{return}
         
         completion(parlamentares, nil)
     }
-    
-    
 }
 
 extension ViewController: UITableViewDelegate{
@@ -57,7 +45,7 @@ extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "deputadosCell") as? DeputadosTableViewCell{
             cell.nomeLabel.text = parlamentares[indexPath.row].txNomeParlamentar
-            cell.valorLabel.text = "\(parlamentares[indexPath.row].vlrDocumento)"
+            cell.valorLabel.text = "R$\((parlamentares[indexPath.row].vlrDocumento * 100).rounded() / 100)"
             
             return cell
         }else{
